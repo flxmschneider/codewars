ea2ce63dc8ec2c0a35bff8ffe1dcaf2830ea4065
Felix <felix-maximilian-schneider@web.de>
2019-02-24 19:06:17 +0100

    upper lowercase is NOT considered now

--- nonRepeatingCharacter/nonRepeatingCharacter.py
+++ nonRepeatingCharacter/nonRepeatingCharacter.py
@@ -1,9 +1,14 @@
+from collections import OrderedDict
+
 def first_non_repeating_letter(string):
-    counter_dict = {}
+    counter_dict = OrderedDict()
     for letter in string:
         if letter in counter_dict.keys():
             counter_dict[letter] +=1
         else:
             counter_dict[letter] = 1
-
     
+    for key, value in counter_dict.items():
+        if value == 1:
+            return key
+    return ''
--- nonRepeatingCharacter/test_nonRepeatingCharacter.py
+++ nonRepeatingCharacter/test_nonRepeatingCharacter.py
@@ -7,19 +7,19 @@ class TestUM(unittest.TestCase):
         pass
  
     def test_1(self):
-        self.assert_equals(first_non_repeating_letter('a'), 'a')
+        self.assertEqual(first_non_repeating_letter('a'), 'a')
     
     def test_2(self):
-        self.assert_equals(first_non_repeating_letter('stress'), 't')
+        self.assertEqual(first_non_repeating_letter('stress'), 't')
     
     def test_3(self):
-        self.assert_equals(first_non_repeating_letter('moonmen'), 'e')
+        self.assertEqual(first_non_repeating_letter('moonmen'), 'e')
     
     def test_4(self):
-        self.assert_equals(first_non_repeating_letter('aa'), '')
+        self.assertEqual(first_non_repeating_letter('aa'), '')
     
     def test_5(self):
-        self.assert_equals(first_non_repeating_letter('~><#~><'), '#')
+        self.assertEqual(first_non_repeating_letter('~><#~><'), '#')
 
 if __name__ == '__main__':
     unittest.main()
