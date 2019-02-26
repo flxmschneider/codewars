a19c214d7571f1138e07439afd832f574cc43deb
Felix <felix-maximilian-schneider@web.de>
2019-02-26 20:54:52 +0100

    tortoiseRacing: started challenge

---
+++ tortoiseRacing/test_tortoiseRacing.py
@@ -0,0 +1,16 @@
+import unittest
+from tortoiuseRacing import race 
+
+class TestUM(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertEqual(race(720, 850, 70), [0, 32, 18])
+
+    def test_2(self):
+        self.assertEqual(race(80, 91, 37), [3, 21, 49])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tortoiseRacing/tortoiseRacing.py
@@ -0,0 +1,2 @@
+def races(v1, v2, g):
+    pass
