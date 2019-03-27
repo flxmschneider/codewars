e44bc606cbae642597e3ea6f761e53ff9b1caabe
Felix <felix-maximilian-schneider@web.de>
2019-03-27 21:32:48 +0100

    started string_incrementer

---
+++ string_incrementer/string_incrementer.py
@@ -0,0 +1,22 @@
+
+def increment_string(string):
+    number = ''
+    for el in string:
+        try:
+            n = int(el)
+            number += el
+        except:
+            continue
+    if number == '':
+        string += '1'
+    else:
+        sub_string = string[0:-len(number)]
+        len_number = len(number)
+        number = int(number)+ 1
+        string_number = str(number)
+        number = '0'*(len_number-len(string_number))+string_number 
+        sub_string += number
+        string = sub_string
+    return string
+            
+            
---
+++ string_incrementer/test_string_incrementer.py
@@ -0,0 +1,17 @@
+import unittest
+from string_incrementer import increment_string 
+
+class Test(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertEqual(increment_string('foo'),'foo1')
+    
+    def test_2(self):
+        self.assertEqual(increment_string('foo01'),'foo02')
+
+
+if __name__ == '__main__':
+    unittest.main()
